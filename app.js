90c4fb8fd5056ce4017034a5d462b32f256de49b
sanjsanj <sanjsanj@hotmail.com>
2015-06-10 19:46:11 +0100

    complete chapter 1

--- app.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  // var gem = { name: 'Azurite', price: '2.95' };
-  var app = angular.module('gemstore', []);
-  app.controller('StoreController', function(){
-    this.product = gem;
-  });
-})();
---
+++ js/app.js
@@ -0,0 +1,11 @@
+(function(){
+  var app = angular.module('gemStore', []);
+  var gems = [
+      { name: 'Azurite', price: 2.95 },
+      { name: 'Bloodstone', price: 5.95 },
+      { name: 'Zircon', price: 3.95 }
+    ];
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
+})();
