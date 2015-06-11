fef89130ed4ae3c6b81260f314ced374bdbab3fc
sanjsanj <sanjsanj@hotmail.com>
2015-06-11 21:48:30 +0100

    complete 2.3

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 (function(){
   var app = angular.module('gemStore', []);
   var gems = [
-      { name: 'Azurite', price: 2.95 },
-      { name: 'Bloodstone', price: 5.95 },
-      { name: 'Zircon', price: 3.95 }
+      { name: 'Azurite', price: 110.50 },
+      { name: 'Bloodstone', price:2.90 },
+      { name: 'Zircon', price: 1100 }
     ];
   app.controller('StoreController', function(){
     this.products = gems;
