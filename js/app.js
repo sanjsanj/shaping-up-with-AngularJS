bdfc3d9d8f776062a0252cec834212fe36d51662
sanjsanj <sanjsanj@hotmail.com>
2015-06-13 22:09:05 +0100

    complete 4.7

--- js/app.js
+++ js/app.js
@@ -5,37 +5,41 @@
     this.products = gems;
   });
 
-  app.controller('GalleryController', function(){
-    this.current = 0;
-
-    this.setCurrent = function(imageNumber){
-      this.current = imageNumber || 0;
-    };
-  });
-
   app.controller("ReviewController", function(){
-
     this.review = {};
 
     this.addReview = function(product){
       product.reviews.push(this.review);
       this.review = {};
     };
+  });
 
-});
+  app.directive('productGallery', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-gallery.html',
+      controller: function(){
+    this.current = 0;
+    this.setCurrent = function(imageNumber){
+      this.current = imageNumber || 0;
+    };
+  },
+      controllerAs: 'gallery'
+    };
+  });
 
-app.directive("productDescription", function(){
+  app.directive("productDescription", function(){
     return {
       restrict: "E",
       templateUrl: "product-description.html"
     };
- });
+  });
 
- app.directive("productSpecs", function(){
-     return {
-       restrict: "A",
-       templateUrl: "product-specs.html"
-     };
+  app.directive("productSpecs", function(){
+    return {
+      restrict: "A",
+      templateUrl: "product-specs.html"
+    };
   });
 
   app.directive("productTabs", function(){
